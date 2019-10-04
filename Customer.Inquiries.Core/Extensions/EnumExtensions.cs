2143399be0e987d6bef0ec881aa395462051a348
Illya Shevchuk <illyai.shevchuk@gmail.com>
2019-10-04 20:37:48 +0300

    extension

---
+++ Customer.Inquiries.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Customer.Inquiries.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<T>(this T value) where T : struct, IConvertible
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            if (fi.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return value.ToString();
+        }
+    }
+}
---
+++ Customer.Inquiries.Core/Extensions/RequiredIfNullAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Inquiries.Core.Extensions
+{
+    public class RequiredIfNullAttribute : ValidationAttribute
+    {
+        private String PropertyName { get; set; }
+        private String ErrorMessage { get; set; }
+
+        public RequiredIfNullAttribute(String propertyName, String errormessage)
+        {
+            this.PropertyName = propertyName;
+            this.ErrorMessage = errormessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Object instance = validationContext.ObjectInstance;
+            Type type = instance.GetType();
+            Object proprtyvalue = type.GetProperty(PropertyName).GetValue(instance, null);
+            if (value == null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}

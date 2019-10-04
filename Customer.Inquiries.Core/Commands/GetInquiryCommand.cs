db16c220b189e8407430704d018396e595eadc62
Illya Shevchuk <illyai.shevchuk@gmail.com>
2019-10-04 20:37:31 +0300

    GetInquiryCommand finished

--- Customer.Inquiries.Core/Commands/GetInquiryCommand.cs
+++ Customer.Inquiries.Core/Commands/GetInquiryCommand.cs
@@ -7,12 +7,18 @@ using Customer.Inquiries.DataAccess.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Customer.Inquiries.Core.Extensions;
 
 namespace Customer.Inquiries.Core.Commands
 {
     public class GetInquiryCommand : IRequest<CustomerProfileDto>
     {
+        [Range(0, 99999, ErrorMessage = "Invalid Customer ID")]
         public int? CustomerId { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        [RequiredIfNull(nameof(GetInquiryCommand.CustomerId), "No inquiry criteria")]
         public string Email { get; set; }
     }
 
@@ -21,17 +27,19 @@ namespace Customer.Inquiries.Core.Commands
         private readonly IRepository repository;
         private readonly IMapper mapper;
 
-        public GetInquiryCommandHandler(IRepository repository)
+        public GetInquiryCommandHandler(IRepository repository, IMapper mapper)
         {
             this.repository = repository;
+            this.mapper = mapper;
         }
 
         public async Task<CustomerProfileDto> Handle(GetInquiryCommand request, CancellationToken cancellationToken)
         {
-            return mapper.Map<CustomerProfileDto>(repository.FindAll<DataAccess.Models.Customer>(x =>
-            (!string.IsNullOrEmpty(request.Email) ? request.Email == x.ContactEmail : true) &&
-            (request.CustomerId.HasValue ? request.CustomerId.Value == x.CustomerId : true))
-                .Include(x => x.Transactions));
+            var items = repository.FindAll<DataAccess.Models.Customer>(x =>
+            (string.IsNullOrEmpty(request.Email) || request.Email == x.ContactEmail) &&
+            (request.CustomerId == null || request.CustomerId.Value == x.CustomerId));
+
+            return mapper.Map<CustomerProfileDto>(await items.Include(x => x.Transactions).FirstOrDefaultAsync());
         }
     }
 }
